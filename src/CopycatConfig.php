fc2e6dbb7669eb7b8062daa765d0eb6280e961a3
Tobias Beßenreither <github@tobias.bessenreither.de>
2026-02-16 23:53:03 +0100

    updated to current Copycat version;

--- src/CopycatConfig.php
+++ src/CopycatConfig.php
@@ -2,15 +2,15 @@
 
 namespace Tbessenreither\MultiLevelCache;
 
-use Tbessenreither\PhpCopycat\Copycat;
 use Tbessenreither\PhpCopycat\Enum\CopyTargetEnum;
 use Tbessenreither\PhpCopycat\Interface\CopycatConfigInterface;
+use Tbessenreither\PhpCopycat\Interface\CopycatInterface;
 
 
 class CopycatConfig implements CopycatConfigInterface
 {
 
-    public static function run(Copycat $copycat): void
+    public static function run(CopycatInterface $copycat): void
     {
         $copycat->copy(
             target: CopyTargetEnum::DDEV_COMMANDS_WEB,
