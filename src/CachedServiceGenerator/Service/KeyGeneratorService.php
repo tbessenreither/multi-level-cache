3f6f25090cbf7c6cc33cc5886d66091bdb25c1b5
Tobias Beßenreither <github@tobias.bessenreither.de>
2026-02-17 00:50:03 +0100

    fixed invalidate issue by providing a $throw argument to getKey to ignore the missing method and just use the defaultKeyGenerator;

--- src/CachedServiceGenerator/Service/KeyGeneratorService.php
+++ src/CachedServiceGenerator/Service/KeyGeneratorService.php
@@ -10,74 +10,80 @@ use Tbessenreither\MultiLevelCache\CachedServiceGenerator\Dto\MethodCallObject;
 
 class KeyGeneratorService
 {
-    /**
-     * @var array<string, callable>
-     */
-    private static array $keyGeneratorCache = [];
-    /**
-     * @var array<string, string>
-     */
-    private static array $cacheKeyPrefixCache = [];
+	/**
+	 * @var array<string, callable>
+	 */
+	private static array $keyGeneratorCache = [];
+	/**
+	 * @var array<string, string>
+	 */
+	private static array $cacheKeyPrefixCache = [];
 
-    public static function getKey(MethodCallObject $methodCallObject): string
-    {
-        $cacheGeneratorCacheKey = $methodCallObject->getCacheGeneratorCacheKey();
+	public static function getKey(MethodCallObject $methodCallObject, bool $throw = true): string
+	{
+		$cacheGeneratorCacheKey = $methodCallObject->getCacheGeneratorCacheKey();
 
-        if (!isset(self::$keyGeneratorCache[$cacheGeneratorCacheKey])) {
-            $reflectionClass = new ReflectionClass($methodCallObject->getClass());
+		if (!isset(self::$keyGeneratorCache[$cacheGeneratorCacheKey])) {
+			$reflectionClass = new ReflectionClass($methodCallObject->getClass());
 
-            if (!$reflectionClass->hasMethod($methodCallObject->getMethod())) {
-                throw new InvalidArgumentException("Method {$methodCallObject->getMethod()} does not exist in class {$methodCallObject->getClass()}"); //TODO: This causes problems on pattern for class invalidation
-            }
+			if (!$reflectionClass->hasMethod($methodCallObject->getMethod())) {
+				if ($throw) {
+					throw new InvalidArgumentException("Method {$methodCallObject->getMethod()} does not exist in class {$methodCallObject->getClass()}");
+				}
+				self::$keyGeneratorCache[$cacheGeneratorCacheKey] = [self::class, 'defaultKeyGenerator'];
 
-            $methodReflection = $reflectionClass->getMethod($methodCallObject->getMethod());
+				// exit to allow invalidate by class to work when $throw is false
+				return self::defaultKeyGenerator($methodCallObject);
+			}
 
-            $mlcCachableMethodAttribute = MlcCachableMethod::fromReflectionMethod($methodReflection);
+			$methodReflection = $reflectionClass->getMethod($methodCallObject->getMethod());
 
-            if ($mlcCachableMethodAttribute->hasKeyGenerator()) {
-                self::$keyGeneratorCache[$cacheGeneratorCacheKey] = $mlcCachableMethodAttribute->getKeyGeneratorCallable();
-            } else {
-                self::$keyGeneratorCache[$cacheGeneratorCacheKey] = [self::class, 'defaultKeyGenerator'];
-            }
-        }
+			$mlcCachableMethodAttribute = MlcCachableMethod::fromReflectionMethod($methodReflection);
 
-        return call_user_func(self::$keyGeneratorCache[$cacheGeneratorCacheKey], $methodCallObject);
-    }
+			if ($mlcCachableMethodAttribute->hasKeyGenerator()) {
+				self::$keyGeneratorCache[$cacheGeneratorCacheKey] = $mlcCachableMethodAttribute->getKeyGeneratorCallable();
+			} else {
+				self::$keyGeneratorCache[$cacheGeneratorCacheKey] = [self::class, 'defaultKeyGenerator'];
+			}
+		}
 
-    public static function getKeyPatternForMethod(MethodCallObject $methodCallObject): string
-    {
-        $generatedFullKey = self::getKey($methodCallObject);
-        $splitKeyParts = explode(':', $generatedFullKey);
-        array_pop($splitKeyParts);
+		return call_user_func(self::$keyGeneratorCache[$cacheGeneratorCacheKey], $methodCallObject);
+	}
 
-        return implode(':', $splitKeyParts) . ':*';
-    }
+	public static function getKeyPatternForMethod(MethodCallObject $methodCallObject): string
+	{
+		$generatedFullKey = self::getKey($methodCallObject);
+		$splitKeyParts = explode(':', $generatedFullKey);
+		array_pop($splitKeyParts);
 
-    public static function getKeyPatternForClass(MethodCallObject $methodCallObject): string
-    {
-        $generatedFullKey = self::getKey($methodCallObject);
-        $splitKeyParts = explode(':', $generatedFullKey);
-        array_pop($splitKeyParts);
-        array_pop($splitKeyParts);
+		return implode(':', $splitKeyParts) . ':*';
+	}
 
-        return implode(':', $splitKeyParts) . ':*';
-    }
+	public static function getKeyPatternForClass(MethodCallObject $methodCallObject): string
+	{
+		$generatedFullKey = self::getKey($methodCallObject, false);
+		$splitKeyParts = explode(':', $generatedFullKey);
+		array_pop($splitKeyParts);
+		array_pop($splitKeyParts);
 
-    public static function defaultKeyGenerator(MethodCallObject $methodCallObject): string
-    {
-        if (!isset(self::$cacheKeyPrefixCache[$methodCallObject->getClass()])) {
-            $cachedClassString = $methodCallObject->getClass() . 'Cached';
-            $cachedServiceReflection = new ReflectionClass($cachedClassString);
-            $cacheKeyPrefix = $cachedServiceReflection->getConstant('CACHE_KEY_PREFIX');
-            if ($cacheKeyPrefix === false) {
-                throw new InvalidArgumentException("CACHE_KEY_PREFIX constant not found in class {$cachedClassString}");
-            }
-            self::$cacheKeyPrefixCache[$methodCallObject->getClass()] = $cacheKeyPrefix;
-        } else {
-            $cacheKeyPrefix = self::$cacheKeyPrefixCache[$methodCallObject->getClass()];
-        }
+		return implode(':', $splitKeyParts) . ':*';
+	}
 
-        return $cacheKeyPrefix . ':' . $methodCallObject->getMethod() . ':' . sha1(serialize($methodCallObject->getArguments()));
-    }
+	public static function defaultKeyGenerator(MethodCallObject $methodCallObject): string
+	{
+		if (!isset(self::$cacheKeyPrefixCache[$methodCallObject->getClass()])) {
+			$cachedClassString = $methodCallObject->getClass() . 'Cached';
+			$cachedServiceReflection = new ReflectionClass($cachedClassString);
+			$cacheKeyPrefix = $cachedServiceReflection->getConstant('CACHE_KEY_PREFIX');
+			if ($cacheKeyPrefix === false) {
+				throw new InvalidArgumentException("CACHE_KEY_PREFIX constant not found in class {$cachedClassString}");
+			}
+			self::$cacheKeyPrefixCache[$methodCallObject->getClass()] = $cacheKeyPrefix;
+		} else {
+			$cacheKeyPrefix = self::$cacheKeyPrefixCache[$methodCallObject->getClass()];
+		}
+
+		return $cacheKeyPrefix . ':' . $methodCallObject->getMethod() . ':' . sha1(serialize($methodCallObject->getArguments()));
+	}
 
 }
