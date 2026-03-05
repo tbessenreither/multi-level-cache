737a572252ad7a76d28a6555cc75975a451fb1cb
Tobias Beßenreither <github@tobias.bessenreither.de>
2026-03-05 20:09:20 +0100

    feature(CachedServiceGenerator): the Class and Method Attributes now support a dataVersion argument. This allows to differentiate between versions when using a shared redis Service

--- src/CachedServiceGenerator/Attribute/MlcCacheableService.php
+++ src/CachedServiceGenerator/Attribute/MlcCacheableService.php
@@ -6,19 +6,34 @@ namespace Tbessenreither\MultiLevelCache\CachedServiceGenerator\Attribute;
 
 use Tbessenreither\MultiLevelCache\CachedServiceGenerator\Service\MakeCachedServiceService;
 use Attribute;
+use ReflectionClass;
 
 /**
  * To mark a service as cacheable.
  */
 #[Attribute(Attribute::TARGET_CLASS)]
+
+
 class MlcCacheableService
 {
     private int $defaultTtlSeconds;
+
     public function __construct(
         ?int $defaultTtlSeconds = null,
         private ?string $additionalInterface = null,
+        private ?string $dataVersion = null,
     ) {
         $this->defaultTtlSeconds = $defaultTtlSeconds ?? MakeCachedServiceService::DEFAULT_TTL_SECONDS;
+    }
+
+    public static function fromReflectionClass(ReflectionClass $class): ?self
+    {
+        $attributes = $class->getAttributes(self::class);
+        if (empty($attributes)) {
+            return null;
+        }
+
+        return $attributes[0]->newInstance();
     }
 
     public function getDefaultTtlSeconds(): int
@@ -29,6 +44,11 @@ class MlcCacheableService
     public function getAdditionalInterface(): ?string
     {
         return $this->additionalInterface;
+    }
+
+    public function getDataVersion(): ?string
+    {
+        return $this->dataVersion;
     }
 
 }
