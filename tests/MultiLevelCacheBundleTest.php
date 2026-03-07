ca16b5b08c0c990692582b80c84e62dab838f289
Tobias Beßenreither <github@tobias.bessenreither.de>
2026-03-08 00:32:56 +0100

    bugfix(Bundle): moved definitions from Compiler Pass to Bundle; (#9)
    * bugfix(Bundle): moved definitions from Compiler Pass to Bundle;

--- tests/MultiLevelCacheBundleTest.php
+++ tests/MultiLevelCacheBundleTest.php
@@ -4,11 +4,17 @@ declare(strict_types=1);
 
 namespace Tbessenreither\MultiLevelCache\Tests;
 
+use Generator;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
+use Tbessenreither\MultiLevelCache\CachedServiceGenerator\Service\InvalidatorService;
+use Tbessenreither\MultiLevelCache\DataCollector\MultiLevelCacheDataCollector;
 use Tbessenreither\MultiLevelCache\DependencyInjection\Compiler\CompilerPass;
+use Tbessenreither\MultiLevelCache\Factory\MultiLevelCacheFactory;
 use Tbessenreither\MultiLevelCache\MultiLevelCacheBundle;
 
 #[CoversClass(MultiLevelCacheBundle::class)]
@@ -53,6 +59,72 @@ class MultiLevelCacheBundleTest extends TestCase
             ->method('addCompilerPass');
 
         $compilerPass->build($this->containerBuilder);
+    }
+
+    #[DataProvider('classesAreRegisteredDataProvider')]
+    public function testClassesAreRegistered(bool $hasDefinition): void
+    {
+        $expectedClasses = self::expectedClassesProvider();
+
+        $compilerPass = new MultiLevelCacheBundle();
+
+        $this->containerBuilder
+            ->expects($this->once())
+            ->method('isCompiled')
+            ->willReturn(false);
+
+        $this->containerBuilder
+            ->expects($this->atLeast(count($expectedClasses)))
+            ->method('hasDefinition')
+            ->willReturn($hasDefinition);
+
+        $submittedClasses = [];
+
+        if (!$hasDefinition) {
+            $this->containerBuilder
+                ->expects($this->atLeast(count($expectedClasses)))
+                ->method('setDefinition')
+                ->willReturnCallback(function (string $id, Definition $definition) use (&$submittedClasses) {
+                    $submittedClasses[] = $id;
+                    return $definition;
+                });
+        } else {
+            $definitionMock = $this->createMock(Definition::class);
+            $definitionMock->expects($this->atLeast(count($expectedClasses)))
+            ->method('setAutowired');
+            $definitionMock->expects($this->atLeast(count($expectedClasses)))
+            ->method('setAutoconfigured');
+            $definitionMock->expects($this->atLeast(count($expectedClasses)))
+            ->method('setPublic');
+
+            $this->containerBuilder
+                ->expects($this->atLeast(count($expectedClasses)))
+                ->method('getDefinition')
+                ->willReturnCallback(function (string $id) use ($definitionMock, &$submittedClasses) {
+                    $submittedClasses[] = $id;
+                    return $definitionMock;
+                });
+        }
+
+        $compilerPass->build($this->containerBuilder);
+        foreach ($expectedClasses as $expectedClass) {
+            $this->assertContains($expectedClass, $submittedClasses, "Expected class $expectedClass was not processed by Compiler Pass.");
+        }
+    }
+
+    public static function classesAreRegisteredDataProvider(): Generator
+    {
+        yield 'Classes are not registered' => [false];
+        yield 'Classes are already registered' => [true];
+    }
+
+    public static function expectedClassesProvider(): array
+    {
+        return [
+            MultiLevelCacheFactory::class,
+            InvalidatorService::class,
+            MultiLevelCacheDataCollector::class,
+        ];
     }
 
 }
