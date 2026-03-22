8b2c8075fb351a7cbd0be2eec8791c387f65b9f8
Tobias Beßenreither <github@tobias.bessenreither.de>
2026-03-22 12:55:03 +0100

    feat(CSG): the CSG now auto transfers all interfaces from the Source Service to the Cached Service;

---
+++ tests/Commands/TestFiles/Interface/Service/InterfaceA.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tbessenreither\MultiLevelCache\Tests\Commands\TestFiles\Interface\Service;
+
+interface InterfaceA
+{
+    public function methodA(): string;
+}
---
+++ tests/Commands/TestFiles/Interface/Service/InterfaceB.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tbessenreither\MultiLevelCache\Tests\Commands\TestFiles\Interface\Service;
+
+interface InterfaceB
+{
+    public function methodB(): string;
+}
